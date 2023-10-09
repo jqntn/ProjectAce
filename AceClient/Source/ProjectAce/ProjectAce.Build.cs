327b0717639962ef16b68ab6f1343d635683940a
Julien QUENTIN <jqntn88@gmail.com>
2023-10-10 00:17:04 +0200

    add enet client + update materials

--- AceClient/Source/ProjectAce/ProjectAce.Build.cs
+++ AceClient/Source/ProjectAce/ProjectAce.Build.cs
@@ -8,7 +8,7 @@ public class ProjectAce : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ENet6Library" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
