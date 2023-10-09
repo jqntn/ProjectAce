ebad9f2fff04af3c2435d77f466f647c4d947eed
Julien QUENTIN <jqntn88@gmail.com>
2023-10-09 23:42:26 +0200

    create dummy class

---
+++ AceClient/Source/ProjectAce/ProjectAce.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectAce.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectAce, "ProjectAce" );
