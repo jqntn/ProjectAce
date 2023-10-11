6ed4557edfb8d8207a01308ac8b08d35f45d3fcf
Str0ke_ <charlelie.garrigues@gmail.com>
2023-10-11 11:58:19 +0200

    Fixes on utils

--- AceServer/src/include/math/utils.hpp
+++ AceServer/src/include/math/utils.hpp
@@ -4,6 +4,29 @@
 
 // Ensemble de fonctions utilitaires
 
+/** Returns higher value in a generic way */
+template< class T >
+static constexpr T Max(const T A, const T B)
+{
+	return (B < A) ? A : B;
+}
+
+/** Returns lower value in a generic way */
+template< class T >
+static constexpr T Min(const T A, const T B)
+{
+	return (A < B) ? A : B;
+}
+
+
+
+/** Clamps X to be between Min and Max, inclusive */
+template< class T >
+constexpr T Clamp(const T X, const T MinValue, const T MaxValue)
+{
+	return Max(Min(X, MaxValue), MinValue);
+}
+
 // Le calcul des flottants n'étant pas exact, on utilise un epsilon plutôt que
 // zéro pour vérifier certains calculs
 template<typename T>
@@ -31,28 +54,8 @@ auto SmoothLerp(T1  Current, T2 Target, T3 DeltaTime, T4 InterpSpeed)
 	}
 
 	// Delta Move, Clamp so we do not over shoot.
-	const RetType DeltaMove = Dist * FMath::Clamp<RetType>(DeltaTime * InterpSpeed, 0.f, 1.f);
+	const RetType DeltaMove = Dist * Clamp<RetType>(DeltaTime * InterpSpeed, 0.f, 1.f);
 
 	return Current + DeltaMove;
 }
 
-
-/** Returns higher value in a generic way */
-template< class T >
-static constexpr T Max(const T A, const T B)
-{
-	return (B < A) ? A : B;
-}
-
-/** Returns lower value in a generic way */
-template< class T >
-static constexpr T Min(const T A, const T B)
-{
-	return (A < B) ? A : B;
-}
-
-/** Clamps X to be between Min and Max, inclusive */
-constexpr T Clamp(const T X, const T MinValue, const T MaxValue)
-{
-	return Max(Min(X, MaxValue), MinValue);
-}
